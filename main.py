1aaf73893ec468c1d6b3ba6882bed15b15e478dd
Paul-Marina <marinafitp@gmail.com>
2021-11-01 18:42:31 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 def verif_prim(n):
     """
-    Verifica daca un numar introduc este prim sau nu
+    Verifica daca un numar introdus este prim sau nu
     :param n: numar natural
     :return: True daca numarul introdus e prim, iar False in caz contrar
     """
@@ -11,27 +10,37 @@ def verif_prim(n):
         return True
     elif n % 2 == 0 :
         return False
-    for i in range (3,n//2):
+    for i in range(3, n//2):
         if n % i == 0 :
             return False
     return True
 
 def  get_longest_sum_is_prime(list):
     """
-    Verifica daca suma numerelor este număr prim.
-    :param list: numere naturale
-    :return: True daca suma este un numar prim, iar False in caz contrar
+    Determinare cea mai lungă subsecvență de numere ce au suma un nr prim
+    :param list: lista cu numere intregi
+    :return: Cea mai lungă subsecvență de numere ce au suma un nr prim
     """
-    sumaelem=0
+    rezultat=[]
+    s=0
+    l = 0
+    maxim = 0
     for i in range(len(list)):
-        sumaelem=sumaelem+list[i]
-    if verif_prim(sumaelem) == True:
-        return True
-    else :
-        return False
+        s = 0
+        for j in range(i, len(list)):
+                s = s + list[j]
+                if verif_prim(s):
+                    if j-i+1 > maxim:
+                        maxim = j - i + 1
+                        l = i
+        for i in range(l,l+maxim):
+            rezultat.append(list[i])
+        return rezultat
+
 def test_get_longest_sum_is_prime():
-    assert get_longest_sum_is_prime([1, 3, 3]) is True
-    assert get_longest_sum_is_prime([1, 2, 3]) is False
+    assert get_longest_sum_is_prime([2, 3, 7]) == [2, 3]
+    assert get_longest_sum_is_prime([1, 2, 3]) == [1, 2]
+
 def nrcifre(n):
     """
     Calculez nr de cifre a numarului introdus
@@ -46,33 +55,55 @@ def nrcifre(n):
     return nr
 def get_longest_digit_count_desc(list):
     """
-    Verifica daca numerele alaturate dintr o lista au numarul de cifre in ordine descrescatoare
+    determinare cea mai lungă subsecvență de numere ce numarul de cifre in ordine descrescatoare
     :param list: numere naturale
-    :return: daca numerele alaturate dintr o lista au numarul de cifre in ordine descrescatoare afiseaza True, in caz contrar False
+    :return: Cea mai lungă subsecvență de numere ce numarul de cifre in ordine descrescatoare
     """
-    for i in range(len(list)-1):
-        if nrcifre(list[i]) < nrcifre(list[i+1]):
-            return False
-    return True
+    l=0
+    rezul=[]
+    maxim = 0
+    for i in range(len(list)):
+        for j in range(i+1, len(list)):
+             if nrcifre(list[j-1]) > nrcifre(list[j]):
+                 if j - i + 1 > maxim:
+                     maxim = j - i + 1
+                     l = i
+             else:
+                 break
+    for i in range(l, l + maxim):
+        rezul.append(list[i])
+    return rezul
+
 
 def test_get_longest_digit_count_desc():
-    assert get_longest_digit_count_desc([123, 12, 1]) is True
-    assert get_longest_digit_count_desc([123, 12, 123]) is False
+    assert get_longest_digit_count_desc([123, 12, 1]) == [123, 12, 1]
+    assert get_longest_digit_count_desc([123, 12, 123]) == [123, 12]
 
 def get_longest_sorted_asc(list):
     """
-    verifica daca numerele din lista sunt in ordine crescatoare
+    Determina cea mai lungă subsecvență de numere care sunt in ordine crescatoare
     :param list: numere naturale
-    :return: True daca numerele sunt ordonate crescator, False in caz contrat
+    :return: Cea mai lungă subsecvență de numere care sunt in ordine crescatoare
     """
-    for i in range(len(list)-1):
-        if list[i] > list[i+1]:
-            return False
-    return True
+    rezu=[]
+    l=0
+    maxim = 0
+    for i in range(len(list)):
+        for j in range(i + 1, len(list)):
+            if list[j - 1] < list[j]:
+                if j - i + 1 > maxim:
+                    maxim = j - i + 1
+                    l = i
+            else:
+                break
+    for i in range(l, l + maxim):
+        rezu.append(list[i])
+        return rezu
 
 def test_get_longest_sorted_asc():
-    assert get_longest_sorted_asc([1, 2, 3]) is True
-    assert get_longest_sorted_asc([1, 3, 2]) is False
+    assert get_longest_sorted_asc([1, 2, 3]) == [1, 2, 3]
+    assert get_longest_sorted_asc([1, 3, 2]) == [1, 3]
+
 def main():
     while True:
         list = []
@@ -82,9 +113,9 @@ def main():
         print(test_get_longest_sum_is_prime())
         print(test_get_longest_digit_count_desc())
         print(test_get_longest_sorted_asc())
-        print("1.verifica daca suma numerelor este număr prim.")
-        print("2.verifica daca numărul de cifre ale numerelor din lista este în ordine descrescătoare.")
-        print("3.verifica daca numerele sunt ordonate crescator.")
+        print("1.Afiseaza cea mai lungă subsecvență de numere ce au suma un nr prim.")
+        print("2.Afiseaza cea mai lungă subsecvență de numere ce au numarul de cifre in ordine descrescatoare.")
+        print("3.Afiseaza cea mai lungă subsecvență de numere care sunt in ordine crescatoare.")
         print("x.iesire")
         optiune = input("dati optiune : ")
         if optiune == "1":
@@ -94,8 +125,9 @@ def main():
         elif optiune == "3":
            print(get_longest_sorted_asc(list))
         elif optiune == "x":
-         break
+            break
         else :
-         print("Optiune gresita, reincercati.")
-if main():
-    main()
+            print("Optiune gresita, reincercati.")
+
+
+main()
